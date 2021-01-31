2ca1cfe2fa6b6f7eba91f9b9f0f752dca3f9e7ee
mainlyetcetera <ericcampbell294@gmail.com>
2021-01-31 10:26:13 -0700

    Add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+// App component, to hold list of ideas
+// Form component, create new ideas, add ideas to App's list
+// Card component to display ideas
+// Ideas component to hold the cards
+
+import React, { Component } from 'react';
+import Ideas from './Ideas';
+import Form from './Form';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ideas: []
+    }
+  }
+
+  addIdea = newIdea => {
+    this.setState({ ideas: [...this.state.ideas, newIdea] });
+  }
+
+  deleteIdea = id => {
+    const filteredIdeas = this.state.ideas.filter(idea => idea.id !== id);
+    this.setState({ ideas: filteredIdeas });
+  }
+
+  render = () => (
+    <main className='App'>
+      <h1>IdeaBox</h1>
+      { !this.state.ideas.length && <h2>No ideas yet -- add some!</h2> }
+      <Form addIdea={this.addIdea} />
+      <Ideas ideas={this.state.ideas} deleteIdea={this.deleteIdea} />
+    </main>
+  )
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Card.css';
+
+const Card = ({ title, description, id, deleteIdea }) => (
+  <div className='card'>
+    <h3>{title}!</h3>
+    <p>{description}</p>
+    <button onClick={() => deleteIdea(id)}>🗑</button>
+  </div>
+)
+
+export default Card;
